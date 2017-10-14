bf3dc70db95b746ac924f4ff05d7652ae58a4a22
QiMonth <0739hunan@sina.com>
2017-10-14 10:29:55 +0800

    web push

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,20 @@
 importScripts('https://www.gstatic.com/firebasejs/4.5.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.5.1/firebase-messaging.js'); 
+importScripts('https://www.gstatic.com/firebasejs/4.5.1/firebase-messaging.js');
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAKW6Aul80UFW2LZcgcyoh4AaeszH943Z4",
-    authDomain: "qimonth.firebaseapp.com",
-    databaseURL: "https://qimonth.firebaseio.com",
-    projectId: "qimonth",
-    storageBucket: "qimonth.appspot.com",
-    messagingSenderId: "561356343570"
+    apiKey: "AIzaSyDHZTrl00SP05ao8jsj1E80gFV2LC2x-fQ",
+    authDomain: "gcall-4a0f8.firebaseapp.com",
+    databaseURL: "https://gcall-4a0f8.firebaseio.com",
+    projectId: "gcall-4a0f8",
+    storageBucket: "gcall-4a0f8.appspot.com",
+    messagingSenderId: "851484560417"
 };
 
 firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
-// messaging.setBackgroundMessageHandler(payload => {
-//     console.log('[worker] Received push notification: ', payload);
-//     return self.registration.showNotification(payload.title, payload);
-// });
-
-
+messaging.setBackgroundMessageHandler(payload => {
+    console.log('[worker] Received push notification: ', payload);
+    return self.registration.showNotification(payload.title, payload);
+});
